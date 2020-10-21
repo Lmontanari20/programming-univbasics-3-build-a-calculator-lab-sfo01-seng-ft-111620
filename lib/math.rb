5b7bebd40b72d6cee544039ab95779161aab4664
Lucas Montanari <lmonty2011@gmail.com>
2020-10-21 17:26:47 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,21 @@
 def addition(num1, num2)
-  Math.addition(num1,num2)
+  num1+num2
 end
 
 def subtraction(num1, num2)
-  Math.subtraction(num1,num2)
+  num1-num2
 end
 
 def division(num1, num2)
-  Math.division(num1,num2)
+  num1/num2
 end
 
 def multiplication(num1, num2)
-  Math.multiplication(num1,num2)
+  num1*num2
 end
 
 def modulo(num1, num2)
-  Math.modulo(num1,num2)
+  num1%num2
 end
 
 def square_root(num)
